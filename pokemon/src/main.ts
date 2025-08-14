430e3497d989816a85143b3ceea0a9f32daf2aaf
MinChatr <min_etc@hotmail.com>
2025-08-14 11:09:16 +0700

    comment main.ts which connect with App.vue and index.html

--- pokemon/src/main.ts
+++ pokemon/src/main.ts
@@ -1,11 +1,18 @@
+// import global styles 
 import './assets/main.css'
 
+// import function vue - createApp
 import { createApp } from 'vue'
+// import component หลัก (root component)
 import App from './App.vue'
+// import ตัวจัดการเส้นทาง (Vue Router) src/router/index.ts
 import router from './router'
 
+// สร้าง app 
 const app = createApp(App)
 
+// บอก app ให้รู้จักระบบ routing
 app.use(router)
 
+// mount คือบอกให้ vue เอา app ไปแสดงบนหน้าเว็บจริง | ติดตั้ง app Vue ลงไปใน element ที่มี id="app"
 app.mount('#app')
